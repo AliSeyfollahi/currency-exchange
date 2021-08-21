e4953610b69d6afd333e4e7f32344a50edd3b990
Seyfollahi <ali.seyfollahi@gmail.com>
2021-08-21 23:34:52 +0430

    fix: fix minor issues

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
-import { Container, Tab, Tabs, useMediaQuery } from "@material-ui/core";
+import { Container, Grow, Tab, Tabs, useMediaQuery } from "@material-ui/core";
 import { FindReplace } from "@material-ui/icons";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link, useLocation } from "react-router-dom";
 
@@ -17,46 +17,55 @@ export default function Header() {
     setValue(value > 0 ? value : 1);
   }, [location]);
 
+  useEffect(() => {
+    //-----to update tab indicator
+    window.setTimeout(() => {
+      window.dispatchEvent(new CustomEvent("resize"));
+    }, 1000);
+  }, []);
+
   return (
     <div className="border-b-2">
       <Container className="flex items-center">
-        <Tabs
-          aria-label="main tabs"
-          value={value}
-          indicatorColor="primary"
-          textColor="primary"
-          orientation={mediumViewport ? "horizontal" : "vertical"}
-          style={{ width: "100%" }}
-        >
-          <Tab
-            label={
-              <label className="flex items-center pb-1">
-                <FindReplace color="primary" className="mr-1" />
-                <h1 className="text-xl">
-                  <span className="capitalize inline-block">
-                    {t("main.title_p1")}
-                  </span>
-                  <span className="capitalize inline-block font-semibold">
-                    {t("main.title_p2")}
-                  </span>
-                </h1>
-              </label>
-            }
-            className="pointer-events-none pl-0 mr-3 max-w-full w-full md:w-auto"
-          />
-          <Link
-            to="/"
-            className="max-w-full"
-            component={Tab}
-            label={<h2>{t("home.currency_converter")}</h2>}
-          />
-          <Link
-            to="/ConversionHistory"
-            className="max-w-full"
-            component={Tab}
-            label={<h2>{t("home.view_conversion_history")}</h2>}
-          />
-        </Tabs>
+        <Grow in={true}>
+          <Tabs
+            aria-label="main tabs"
+            value={value}
+            indicatorColor="primary"
+            textColor="primary"
+            orientation={mediumViewport ? "horizontal" : "vertical"}
+            style={{ width: "100%" }}
+          >
+            <Tab
+              label={
+                <label className="flex items-center pb-1">
+                  <FindReplace color="primary" className="mr-1" />
+                  <h1 className="text-xl">
+                    <span className="capitalize inline-block">
+                      {t("main.title_p1")}
+                    </span>
+                    <span className="capitalize inline-block font-semibold">
+                      {t("main.title_p2")}
+                    </span>
+                  </h1>
+                </label>
+              }
+              className="pointer-events-none pl-0 mr-3 max-w-full w-full md:w-auto"
+            />
+            <Link
+              to="/"
+              className="max-w-full"
+              component={Tab}
+              label={<h2>{t("home.currency_converter")}</h2>}
+            />
+            <Link
+              to="/ConversionHistory"
+              className="max-w-full"
+              component={Tab}
+              label={<h2>{t("home.view_conversion_history")}</h2>}
+            />
+          </Tabs>
+        </Grow>
       </Container>
     </div>
   );
